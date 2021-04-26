0d9adb59bf98d8e5427f220e5297052135ff5bbc
Andrei <corbu.andrei@yahoo.com>
2021-04-26 20:34:04 +0300

    added template

---
+++ template.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstring>
+#include <fstream>
+#include <cmath>
+using namespace std;
+ifstream fin("bac.txt");
+// ofstream fout("out.txt");
+#define min(a,b) ((a)<(b)?(a):(b))
+#define max(a,b) ((a)>(b)?(a):(b))
+int main(){
+    
+    return 0;
+}
