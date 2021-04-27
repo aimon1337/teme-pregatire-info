604a8362ad91e3b9e230cb2bb8e27241fb00ef21
Andrei <corbu.andrei@yahoo.com>
2021-04-27 12:57:27 +0300

    added test antrenament 8

---
+++ tema-4-mai/testantr.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstring>
+#include <fstream>
+#include <cmath>
+using namespace std;
+ifstream fin("bac.txt");
+// ofstream fout("out.txt");
+#define min(a,b) ((a)<(b)?(a):(b))
+#define max(a,b) ((a)>(b)?(a):(b))
+
+/*
+S1:
+1-c
+2-d
+3-b
+4-c
+5-
+*/
+
+int main(){
+
+    return 0;
+}
