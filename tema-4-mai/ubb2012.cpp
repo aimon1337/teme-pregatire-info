8d7c528ed76b157bbf90d900f32b6d0d9679f6b3
Andrei <corbu.andrei@yahoo.com>
2021-05-03 20:25:51 +0300

    fixed ubb2012

--- tema-4-mai/ubb2012.cpp
+++ tema-4-mai/ubb2012.cpp
@@ -34,48 +34,64 @@ int asemenea(int x, int y){
     }
     return 1;
 }
-void eliminare(int x[100], int &n, int poz){
-    for(int i=poz; i<=n; ++i){
-        x[i]=x[i+1];
+void eliminare(int v[], int &n, int x, int y){
+    for(int i = 1; i <= y-x+1; ++i){
+        for(int j=x; j<n; ++j){
+            v[j]=v[j+1];
+        }
     }
-    n--;
+    n=n-(y-x+1);
+}
+int cautare(int x[], int n, int k){
+    for(int i=1 ;i<=n; ++i){
+        if(x[i]==k) return 1;
+    }
+    return 0;
+}
+void build(int x[], int n, int r[], int &m){
+    int i,j;
+    for(i=1;i<n;++i){
+        if(asemenea(x[i],x[i+1])){
+            j=i+1;
+            while(asemenea(x[i],x[j]) && j<=n){
+                j++;
+            }
+            for(int k=i; k<j; ++k){
+                if(cautare(r,m,x[k])==0) r[++m]=x[k];
+            }
+        }
+    }
+}
+void eliminaresecv(int x[], int &n){
+    for(int i=1;i<n;++i){
+        if(asemenea(x[i],x[i+1])){
+            int j=i+1;
+            while(asemenea(x[i],x[j]) && j<=n){
+                j++;
+            }
+            eliminare(x,n,i,j-1);
+            i--;
+        }
+    }
 }
 void print(int x[100], int n){
-    if(n==0) cout<<"Sir vid";
-    else
     for(int i=1;i<=n;++i){
         cout<<x[i]<<" ";
     }
 }
-void creare(int x[100], int &n){
-    int i,j, R[100],poz=0;
-    for(i=1; i<=n-1; ++i){
-        bool ok=false;
-        for(j=i+1; j<=n; ++j){
-            if(x[i]==x[j]){
-                R[++poz]=x[i];
-                eliminare(x, n, j);
-                ok=false;
-            }
-            else if (asemenea(x[i], x[j])==1){
-                    R[++poz]=x[j];
-                    eliminare(x,n,j);
-                    ok=true;
-                }
-        }
-        if(ok==true){
-            R[++poz]=x[i];
-        }
-    }
-    cout<<"X= ";
-    print(x,n);
-    cout<<endl;
-    print(R,poz);
-
-}
 int main(){
-    int x[100], n=0;
+    int x[200], r[200], n=0,m=0;
     citire(x,n);
-    creare(x,n);
+    build(x,n,r,m);
+    cout<<"\n";
+    eliminaresecv(x,n);
+    if(n>0){
+        print(x,n);
+        cout<<endl;
+    }
+    else    cout<<"X e vid\n";
+    if(m>0)
+        print(r,m);
+    else    cout<<"R e vid\n";
     return 0;
 }
